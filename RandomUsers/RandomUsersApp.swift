d4e8f18c412a97f4f8f7cd4fa1414ae1aee83bff
Adam Cseke <cseke.adam@me.com>
2026-01-03 12:49:37 +0100

    initial commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,19 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Adam Cseke on 2025. 12. 30..
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                UserListView()
+            }
+        }
+    }
+}
---
+++ RandomUsersTests/RandomUsersTests.swift
@@ -0,0 +1,16 @@
+//
+//  RandomUsersTests.swift
+//  RandomUsersTests
+//
+//  Created by Adam Cseke on 2025. 12. 30..
+//
+
+import Testing
+
+struct RandomUsersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
