99f756f976e89c6d4b282effc6d9e77be1fc3663
Adam Cseke <cseke.adam@me.com>
2026-01-03 17:16:44 +0100

    Remove .DS_Store files from repository history

---
+++ App/Screens/UserList/Views/UserRow.swift
@@ -0,0 +1,18 @@
+//
+//  UserRow.swift
+//  RandomUsers
+//
+//  Created by Adam Cseke on 2026. 01. 03..
+//
+
+import SwiftUI
+
+struct UserRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserRow()
+}
